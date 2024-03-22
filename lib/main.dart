37e61e1331ca763dc5a3af9093948d4436450dda
dgomesdev <danilo.gomes@outlook.fr>
2024-03-22 08:54:30 -0300

    fix: Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_notes/firebase_options.dart';
+import 'package:my_notes/firebaseOptions/firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
