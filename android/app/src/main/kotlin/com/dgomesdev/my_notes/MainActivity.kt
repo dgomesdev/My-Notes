1ca2500945e3a8c1f1211ca7972faf442ba06f4e
dgomesdev <danilo.gomes@outlook.fr>
2024-03-21 22:41:36 -0300

    feat: added Firebase initialization

---
+++ android/app/src/main/kotlin/com/dgomesdev/my_notes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dgomesdev.my_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_notes/firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                              hintText: 'Enter your email'
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false
+                      ),
+                      TextField(
+                          controller: _passwordController,
+                          decoration: const InputDecoration(
+                              hintText: 'Enter your password'
+                          ),
+                          obscureText: true,
+                          autocorrect: false,
+                          enableSuggestions: false
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          final email = _emailController.text;
+                          final password = _passwordController.text;
+                          final userCredential = await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                              email: email,
+                              password: password
+                          );
+                          print(userCredential);
+                        },
+                        child: const Text('Register'),
+                      ),
+                    ],
+                  ),
+                );
+              default:
+                return const Text('Loading...');
+            }
+          }),
+    );
+  }
+}
